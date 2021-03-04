2d523a0132255c04be0b8a8d5ba2511813fa7929
lee gyu han <weaklion@naver.com>
2021-03-04 13:17:03 +0900

    제일 작은 수 제거 추가

--- 이진 변환 반복/solution.js
+++ 이진 변환 반복/solution.js
@@ -1,19 +1,7 @@
 function solution(s) {
-  let zeroCount = 0;
-  let loopCount = 0;
-  
-  while( s !== '1') {
-      const tempBinary = [...s].reduce((acc,cur) => {
-          if(cur === '0') {
-              zeroCount ++;
-              return acc;
-          }
-          return acc + 1;
-      },0);
-      
-      s= tempBinary.toString(2);
-      loopCount ++;
+  for(let i=0; i< s; i++) {
+    const stars = '*'.repeat(i*2 + 1);
+    const space = ''.repeat(s - i - 1);
   }
   
-  return [loopCount,zeroCount] ;
 }
